3c68a30a42ecaa8877d238927e4d806b3346542f
xiefei001 <xiefei001@hotmail.com>
2016-06-15 01:08:34 +0200

    update

--- src/main/java/de/swm/ela/presentation/MainLayoutController.java
+++ src/main/java/de/swm/ela/presentation/MainLayoutController.java
@@ -2,6 +2,8 @@ package de.swm.ela.presentation;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import org.springframework.stereotype.Component;
 
 /**
@@ -12,4 +14,14 @@ public class MainLayoutController {
 
     @FXML
     Label helloWorldLabel;
+
+    @FXML
+    RadioButton group1, group2, group3, group4;
+
+    @FXML
+    ToggleGroup myGroup;
+
+    void onChange(){
+        //myGroup.selectedToggleProperty().addListener();
+    }
 }
